d4ba0ce42ffe0cd784d00b0416835aff86ad8e13
Luke Billard <lukebillard04@gmail.com>
2024-01-18 12:04:07 -0330

    big fix with displaying humidity

--- temp_reader.py
+++ temp_reader.py
@@ -8,7 +8,7 @@ def temp_reader(pin_num):
     while True:
         humidity, temp = Adafruit_DHT.read_retry(SENSOR,PIN)
         if humidity is not None and temp is not None:
-            print("Air Temperature: {0:0.1f} C Humidity: {0:0.1f} %".format(temp,humidity))
+            print("Air Temperature: {0:0.1f} C Humidity: {1:0.1f} %".format(temp,humidity))
         else:
             print("Sensor Error")
         time.sleep(4)
