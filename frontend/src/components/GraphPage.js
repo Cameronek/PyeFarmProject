28b84c2f2209e2e212a8da9672bf2a5510dcb2a5
Aaron <71898862+Aaron-Greenland@users.noreply.github.com>
2024-03-12 14:54:53 -0230

    Implement graph function and graph page - broken as of now

---
+++ frontend/src/components/GraphPage.js
@@ -0,0 +1,34 @@
+import React, { Component} from "react";
+import PlantPage from "./PlantPage";
+import {BrowserRouter as Router, Routes, Route, Link, Navigate} from "react-router-dom";
+import SearchPage from "./SearchPage";
+import CameraViewPage from "./CameraViewPage";
+import GraphPage from "./GraphPage";
+import GraphFunction from "./GraphFunction";
+import Grid from "@mui/material/Grid";
+
+// *** HOMEPAGE is effectively an extension of App.js containing page routing
+
+export default class HomePage extends Component{
+    constructor(props){
+        super(props);
+    }
+    render() {
+        return (
+
+            
+          <body style={{margin: "0", padding: 0, backgroundColor:"white", height:"100vh", width:"100vw", position:"relative"}}>
+           
+
+            {/* Call Graph Function */}
+            <Grid container justifyContent="center">
+              <Grid item xs={false} align="center">
+                <GraphFunction></GraphFunction>
+              </Grid>
+            </Grid>
+            
+        </body>
+
+    );
+    }
+}
--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -3,6 +3,7 @@ import PlantPage from "./PlantPage";
 import {BrowserRouter as Router, Routes, Route, Link, Navigate} from "react-router-dom";
 import SearchPage from "./SearchPage";
 import CameraViewPage from "./CameraViewPage";
+import GraphPage from "./GraphPage";
 
 // *** HOMEPAGE is effectively an extension of App.js containing page routing
 
@@ -17,6 +18,8 @@ export default class HomePage extends Component{
                     {/* TODO: Modify paths such that we have individual paths for each plant */}
                     {/* TODO: Dynamic routing and page creation (may not be implemented) */}
                     <Route exact path='/' element={<SearchPage/>}></Route>
+                    <Route exact path='/graph' element={<GraphPage/>}></Route>
+                    <Route exact path='/graph/' element={<GraphPage/>}></Route>
                     <Route exact path='/plant' element={<PlantPage/>}></Route>
                     <Route exact path='/plant/cameraview' element={<CameraViewPage/>}></Route>
                 </Routes>
