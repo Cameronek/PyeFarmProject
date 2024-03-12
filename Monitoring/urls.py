87b843dc76515d2045e660717298ceaa1c81facb
Aaron <71898862+Aaron-Greenland@users.noreply.github.com>
2024-03-12 19:27:50 -0230

    added working graphing function

--- Monitoring/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import MemberView
-
-urlpatterns = [
-    path('/home', MemberView.as_view()),
-]
--- frontend/src/components/GraphPage.js
+++ frontend/src/components/GraphPage.js
@@ -1,4 +1,4 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
 import PlantPage from "./PlantPage";
 import {BrowserRouter as Router, Routes, Route, Link, Navigate} from "react-router-dom";
 import SearchPage from "./SearchPage";
@@ -8,6 +8,7 @@ import GraphFunction from "./GraphFunction";
 import Grid from "@mui/material/Grid";
 
 // *** HOMEPAGE is effectively an extension of App.js containing page routing
+
 
 export default class HomePage extends Component{
     constructor(props){
@@ -19,6 +20,13 @@ export default class HomePage extends Component{
             
           <body style={{margin: "0", padding: 0, backgroundColor:"white", height:"100vh", width:"100vw", position:"relative"}}>
            
+           {/* Test Hello World message */}
+
+            {/* <Grid container justifyContent="center">
+                <Grid item xs={false} align="center">
+                    <h1>Hello World</h1>
+                </Grid>
+            </Grid> */}
 
             {/* Call Graph Function */}
             <Grid container justifyContent="center">
